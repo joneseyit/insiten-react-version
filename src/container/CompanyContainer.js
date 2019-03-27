3a0b02bdeb7bb6458a4ba82d6e58748216767a8f
Andrew Jones <joneseyit@protonmail.com>
2019-03-27 15:50:21 -0400

    setup some framework - yarn installed semantic ui react

---
+++ src/container/CompanyContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CompanyContainer extends Component {
+    render(){
+        return (
+            <h1>Container</h1>
+        )
+    }
+}
+
+export default CompanyContainer
